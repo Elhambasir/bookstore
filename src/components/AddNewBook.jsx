23cfcb013058d5a7facea5883434c90eb18858ee
Elhambasir <108110651+Elhambasir@users.noreply.github.com>
2023-05-12 13:02:56 +0430

    Create AddNewBook.jsx

---
+++ src/components/AddNewBook.jsx
@@ -0,0 +1,23 @@
+import styles from '../styles/addNewBook.module.css'
+const AddNewBook = () => (
+  <>
+   <div className={styles.addNewBookWrapper}>
+    <div className={styles.addNewBookWrapperTitle}>
+      <h3>ADD NEW BOOK</h3>
+    </div>
+    <div className={styles.addNewBookWrapperInputs}>
+    <div className={styles.addNewBookWrapperInputSeprator}>
+    <input type="text" id="title" placeholder="Book Title Here..." />
+    </div>
+    <div className={styles.addNewBookWrapperInputSeprator}>
+    <input type="text" id="name" placeholder="Author Name Here..." />
+    </div>
+    <div className={styles.addNewBookWrapperInputSeprator}>
+    <button type="submit">Add Book</button>
+    </div>
+    </div>
+   </div>
+  </>
+);
+
+export default AddNewBook;
